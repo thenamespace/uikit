18cc0e42d59c7938cdac07065819913ccfbfbb43
arti <nenadmitt@gmail.com>
2025-09-11 19:08:20 +0200

    fix exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
-// re-export higher-level components (organisms/layout) here as they are added
-export { SelectRecordsForm } from "./select-records-form/SelectRecordsForm";
-export { PendingTransaction, TransactionState } from "./pending-transaction/PendingTransaction";
+export * from "./pending-transaction/PendingTransaction";
 export * from "./atoms";
 export * from "./molecules";
+export * from "./ens-records-form/EnsRecordsForm";
+export * from "./select-records-form/SelectRecordsForm";
